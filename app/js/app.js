f988c23b7af3b5e77be6f2b2719ad2f9fd00ceb1
Syrone <cazonove@gmail.com>
2023-05-16 22:40:36 +0300

    experiment countdown

--- app/js/app.js
+++ app/js/app.js
@@ -122,4 +122,108 @@ document.addEventListener('DOMContentLoaded', () => {
 		window.addEventListener('orientationchange', setHeroSwiperPaginationTop);
 	}
 
+	//CLOCK
+	const els = {
+		s: initElements('s'),
+		m: initElements('m'),
+		h: initElements('h')
+	}
+
+	function initElements(type) {
+		const els = [{}, {}]
+
+		if (!['s', 'm', 'h'].includes(type)) return els
+
+		const target = document.querySelector(`.flip-clock-${type}`)
+
+		if (!target) return els
+
+		let el
+
+		el = els[0]
+		el.digit = target.querySelector('.digit-left')
+		el.flip = el.digit.querySelector('.flip')
+		el.flipFaces = el.flip.querySelectorAll('.flip-face')
+		el.flipFaceA = el.flipFaces[0]
+		el.flipFaceB = el.flipFaces[1]
+
+		el = els[1]
+		el.digit = target.querySelector('.digit-right')
+		el.flip = el.digit.querySelector('.flip')
+		el.flipFaces = el.flip.querySelectorAll('.flip-face')
+		el.flipFaceA = el.flipFaces[0]
+		el.flipFaceB = el.flipFaces[1]
+
+		return els
+	}
+
+	(function runClock() {
+		const date = new Date(),
+			now = {
+				h: date.getHours(),
+				m: date.getMinutes(),
+				s: date.getSeconds()
+			}
+		now.h = now.h < 10 ? `0${now.h}` : `${now.h}`
+		now.m = now.m < 10 ? `0${now.m}` : `${now.m}`
+		now.s = now.s < 10 ? `0${now.s}` : `${now.s}`
+		now.h0 = now.h[0]
+		now.h1 = now.h[1]
+		now.m0 = now.m[0]
+		now.m1 = now.m[1]
+		now.s0 = now.s[0]
+		now.s1 = now.s[1]
+
+		console.log(`${now.h0}${now.h1}:${now.m0}${now.m1}:${now.s0}${now.s1}`);
+
+		for (const t of Object.keys(els)) {
+			for (const i of ['0', '1']) {
+				const curr = now[`${t}${i}`]
+				let next = +curr + 1
+				if (t === 'h') {
+					if (i === '0') next = next < 3 ? `${next}` : '0'
+					if (i === '1') next = next < 4 ? `${next}` : '0'
+				}
+				if (t === 'm') {
+					if (i === '0') next = next < 6 ? `${next}` : '0'
+					if (i === '1') next = next < 10 ? `${next}` : '0'
+				}
+				if (t === 's') {
+					if (i === '0') next = next < 3 ? `${next}` : '0'
+					if (i === '1') next = next < 4 ? `${next}` : '0'
+				}
+				const el = els[t][i]
+				if (el && el.digit) {
+					if (!el.digit.dataset.digitBefore) {
+						el.digit.dataset.digitBefore = curr
+						el.flipFaceA.textContent = el.digit.dataset.digitBefore
+						el.digit.dataset.digitAfter = next
+						el.flipFaceB.textContent = el.digit.dataset.digitAfter
+					} else if (el.digit.dataset.digitBefore !== curr) {
+						el.flip.addEventListener('transitionend', function () {
+							el.digit.dataset.digitBefore = curr
+							el.flipFaceA.textContent = el.digit.dataset.digitBefore
+
+							const flipClone = el.flip.cloneNode(true)
+							flipClone.classList.remove('flipped')
+							el.digit.replaceChild(flipClone, el.flip)
+							el.flip = flipClone
+							el.flipFaces = el.flip.querySelectorAll('.flip-face')
+							el.flipFaceA = el.flipFaces[0]
+							el.flipFaceB = el.flipFaces[1]
+
+							el.digit.dataset.digitAfter = next
+							el.flipFaceB.textContent = el.digit.dataset.digitAfter
+						}, { once: true })
+						if (!el.flip.classList.contains('flipped')) {
+							el.flip.classList.add('flipped')
+						}
+					}
+				}
+			}
+		}
+
+		setTimeout(runClock, 1000)
+	})()
+
 });
