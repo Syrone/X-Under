c122227c67d48aadcc40a8ff14bee5202d10f256
Syrone <cazonove@gmail.com>
2023-06-12 13:18:29 +0300

    fix

--- app/js/app.js
+++ app/js/app.js
@@ -150,32 +150,33 @@ document.addEventListener('DOMContentLoaded', () => {
 		} else {
 			alertPromotions.classList.add('show')
 		}
-	}
 
-	alertPromotions.addEventListener('closed.bs.alert', () => {
-		setCookie('alert-promotions-2023', 'true', 1);
-	});
 
-	function setCookie(name, value, days) {
-		const date = new Date();
-		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-		const expires = `expires=${date.toUTCString()}`;
-		document.cookie = `${name}=${value};${expires};path=/`;
-	}
+		alertPromotions.addEventListener('closed.bs.alert', () => {
+			setCookie('alert-promotions-2023', 'true', 1);
+		});
 
-	function getCookie(name) {
-		const nameEQ = `${name}=`;
-		const cookies = document.cookie.split(';');
-		for (let i = 0; i < cookies.length; i++) {
-			let cookie = cookies[i];
-			while (cookie.charAt(0) === ' ') {
-				cookie = cookie.substring(1, cookie.length);
-			}
-			if (cookie.indexOf(nameEQ) === 0) {
-				return cookie.substring(nameEQ.length, cookie.length);
-			}
+		function setCookie(name, value, days) {
+			const date = new Date();
+			date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+			const expires = `expires=${date.toUTCString()}`;
+			document.cookie = `${name}=${value};${expires};path=/`;
 		}
-		return null;
+
+		function getCookie(name) {
+			const nameEQ = `${name}=`;
+			const cookies = document.cookie.split(';');
+			for (let i = 0; i < cookies.length; i++) {
+				let cookie = cookies[i];
+				while (cookie.charAt(0) === ' ') {
+					cookie = cookie.substring(1, cookie.length);
+				}
+				if (cookie.indexOf(nameEQ) === 0) {
+					return cookie.substring(nameEQ.length, cookie.length);
+				}
+			}
+			return null;
+		}
 	}
 
 	//END COOKIE
